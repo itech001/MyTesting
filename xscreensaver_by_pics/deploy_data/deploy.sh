4dcf09835bf6c092cb8c8e2a7e7e3933ade7a771
itech <itech001@126.com>
2014-09-23 10:20:13 -0400

    xscreensaver

--- xscreensaver_by_pics/deploy_data/deploy.sh
+++ xscreensaver_by_pics/deploy_data/deploy.sh
@@ -4,6 +4,7 @@ chmod 777 /mnt/screensaver
 cd /mnt/screensaver
 wget https://github.com/itech001/MyTesting/archive/master.zip
 unzip master.zip
+cp MyTesting/xscreensaver_by_pics ./
 chmod 777 /mnt/screensaver/xscreensaver_by_pics
 
 2) install perl modules
@@ -13,8 +14,6 @@ cpan -fi JSON Data::GUID Sys::HostAddr
 apt-get -y install xscreensaver xscreensaver-gl
 mv /usr/bin/xscreensaver-getimage-file /usr/bin/xscreensaver-getimage-file.bak
 cp  /mnt/screensaver/xscreensaver_by_pics/deploy_data/xscreensaver-getimage-file /usr/bin/xscreensaver-getimage-file 
-mkdir /var/guest-data
-chmod 777 /var/guest-data
 for testing
 /usr/bin/xscreensaver-getimage-file /mnt/screensaver/test
 
@@ -25,7 +24,7 @@ cp /mnt/screensaver/xscreensaver_by_pics/deploy_data/.xscreensaver $SKEL
 #cp $HOME/xscreensaver_by_pics/deploy_data/xscreensaver_service.desktop $SKEL/.config/autostart/xscreensaver_service.desktop
 #cp $HOME/xscreensaver_by_pics/deploy_data/xscreensaver_mytest.desktop $SKEL/.config/autostart/xscreensaver_mytest.desktop
 
-5) autostart to set cron (don't need, will use root cron jobs)
+5) autostart xscreensaver and set cron (don't need, will use root cron jobs)
 mkdir /etc/guest-session
 chmod a+x /etc/guest-session/auto.sh
 auto.sh
@@ -39,14 +38,15 @@ echo start_download
 #(crontab -l 2>/dev/null; echo "*/5 * * * * /mnt/screensaver/xscreensaver_by_pics/deloy_data/download_pics.pl") | crontab -
 
 6)root cron 
+for testing:
+/mnt/screensaver/xscreensaver_by_pics/test_data/start_server.sh
+/mnt/screensaver/xscreensaver_by_pics/deploy_data/download_pics.pl
+
 # m h  dom mon dow   command
 @reboot xscreensaver -no-splash
 @reboot /mnt/screensaver/xscreensaver_by_pics/test_data/start_server.sh
 @reboot /mnt/screensaver/xscreensaver_by_pics/deploy_data/download_pics.pl
 0 9 * * * /mnt/screensaver/xscreensaver_by_pics/deploy_data/download_pics.pl
-for testing:
-/mnt/screensaver/xscreensaver_by_pics/test_data/start_server.sh
-/mnt/screensaver/xscreensaver_by_pics/deploy_data/download_pics.pl
 
 7) set dir permission for guest 
 mkdir /var/guest-data
@@ -55,3 +55,5 @@ vi /etc/apparmor.d/abstractions/lightdm
   /mnt/** rw,
   /var/guest-data/** rw,
 
+8) reboot system to verity result
+xscreensaver-command -activate
